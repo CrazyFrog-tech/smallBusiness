b05469274fc97f90599726baedfb177529ce7bd9
CrayzFrog-tech <allanmohamad7@gmail.com>
2022-02-04 19:01:55 +0100

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import logo from './images/background.jpg'
+
+function App() {
+  return (
+    <div  >
+      <img className='resize' src={logo} alt ="Logo"  />
+    </div>
+  );
+}
+
+export default App;
