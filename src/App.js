aab646cfd57618ad984489c79e6a9c24dfdf813d
CrayzFrog-tech <allanmohamad7@gmail.com>
2022-02-06 19:46:51 +0100

    product boxes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import logo from './images/background.jpg'
+import * as React from 'react';
+import randomColor from "randomcolor";
+
+
 
 function App() {
   return (
-    <div  >
-      <img className='resize' src={logo} alt ="Logo"  />
+    <div >
+      <img className='resize' src={logo} alt="Logo" />,
+      <div class="container">
+
+        <div class="child">
+        </div>
+        <div class="child">
+          <div>Hello</div>
+          <div style={{
+       backgroundColor: randomColor()
+      }} >
+
+          </div>
+        </div>
+        <div class="child">
+
+        </div>
+        <div class="child">
+
+        </div>
+        <div class="child">
+        </div>
+
+      </div>
     </div>
   );
 }
